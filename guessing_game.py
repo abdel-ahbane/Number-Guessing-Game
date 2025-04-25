93af99caa46c61f981903dd79b09cc5943a28abb
Abdelhamid Ahbane <108112325+abdel-ahbane@users.noreply.github.com>
2025-04-25 11:14:34 +0200

    Multiple games with best score challenge
    Added option to play again, and keeps track of best score (min attempts to win)

--- guessing_game.py
+++ guessing_game.py
@@ -21,7 +21,14 @@ Project 1 - The Number Guessing Game
 #      and show how many attempts it took them to get the correct number.
 #   5. Let the player know the game is ending, or something that indicates the game is over.
 
-# ( You can add more features/enhancements if you'd like to. )
+#Option to Play Again Prompt
+#As a player, after I guess correctly and win, I should be prompted if I would like to play again.
+
+#Display a High Score
+#As a player, at the start of each game, I should be shown the current high score (least amount of guess attempts) so that I know what I am supposed to beat.
+
+#Generate a New Winning Number
+#Every time a player decides to play again, the random number to guess is updated so that players are guessing something new each time.
 
 
 # Kick off the program by calling the start_game function.
@@ -39,36 +46,46 @@ def start_game():
     """Prompts the user to input an int guess between 1 to 10,
     then guiding them with a feedback showing it the guess is higher then the winning number or lower until the user gets it right.
     Shows the number of attempts before correct guess.
-    ( More enhancements later... )
+	Keeps count of the best attempts score and allows multiple games to beat best score
     """
     # write your code inside this function.
     welcome_message = "*****WELCOME TO NUMBER GUESSING GAME*****\n\n"
     print(welcome_message)
 
-    jackpot = random.randint(1,10)
-    guess = 0
-    attempts = 0
-	
-    while guess != jackpot:
-        try:
-            guess = int(input("what's your guess (1 - 10)? "))
-            if guess > 10 or guess < 1:
-                raise GuessErrorCustom("the guess should be an integer between 1 and 10")
-        except GuessErrorCustom as e:
-            print(f'invalid input! {e}')
-            continue
-        except ValueError:
-            print("Invalid input! Please enter an integer number.")
-            continue
+	play = 'Y'
+    best_score = 10
 
-        if guess > jackpot:
-            print("it's lower")
-        elif guess < jackpot:
-            print("it's higher")
-        attempts +=1
-    
-    print("\nCongrats, YOU GOT IT!")
-    print(f"It took you {attempts} attempts to guess it correctly")
+	while play.upper() == 'Y':
+		jackpot = random.randint(1,10)
+    	guess = 0
+    	attempts = 0
+		print(f"The best score to beat is {best_score} attempts")
+		
+	    while guess != jackpot:
+	        try:
+	            guess = int(input("what's your guess (1 - 10)? "))
+	            if guess > 10 or guess < 1:
+	                raise GuessErrorCustom("the guess should be an integer between 1 and 10")
+	        except GuessErrorCustom as e:
+	            print(f'invalid input! {e}')
+	            continue
+	        except ValueError:
+	            print("Invalid input! Please enter an integer number.")
+	            continue
+	
+	        if guess > jackpot:
+	            print("it's lower")
+	        elif guess < jackpot:
+	            print("it's higher")
+	        attempts +=1
+
+		if attempts < best_score:
+			best_score = attempts
+		
+		print("\nCongrats, YOU GOT IT!")
+		print(f"It took you {attempts} attempts to guess it correctly")
+
+		play = input("Would you like to play again (Y or N)? ")
 
     exit_message = "\n\n***** GAME OVER - Goodbye! *****"
     print(exit_message)
