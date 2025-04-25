d74d0cc255eb2850a07732681744b69f9cf8c0c3
Abdelhamid Ahbane <108112325+abdel-ahbane@users.noreply.github.com>
2025-04-25 11:35:09 +0200

    Final Commit
    Fixed indentations/spaces and cleared task comments, also adjusted the docstrings.

--- guessing_game.py
+++ guessing_game.py
@@ -1,83 +1,56 @@
 """
-Python Development Techdegree
+Python Development TechDegree
 Project 1 - The Number Guessing Game
 --------------------------------
 """
 
-# Import the random module.
-
-# Create the start_game function.
-# Write your code inside this function.
-
-#   When the program starts, we want to:
-#   ------------------------------------
-#   1. Display an intro/welcome message to the player.
-#   2. Store a random number as the answer/solution.
-#   3. Continuously prompt the player for a guess.
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
-
-#   4. Once the guess is correct, stop looping, inform the user they "Got it"
-#      and show how many attempts it took them to get the correct number.
-#   5. Let the player know the game is ending, or something that indicates the game is over.
-
-#Option to Play Again Prompt
-#As a player, after I guess correctly and win, I should be prompted if I would like to play again.
-
-#Display a High Score
-#As a player, at the start of each game, I should be shown the current high score (least amount of guess attempts) so that I know what I am supposed to beat.
-
-#Generate a New Winning Number
-#Every time a player decides to play again, the random number to guess is updated so that players are guessing something new each time.
-
-
-# Kick off the program by calling the start_game function.
 
 import random
 
 
 class GuessErrorCustom(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+	"""Custom exception to handle invalid guesses."""
+	def __init__(self, message):
+		self.message = message
+		super().__init__(self.message)
 
 
 def start_game():
-    """Prompts the user to input an int guess between 1 to 10,
-    then guiding them with a feedback showing it the guess is higher then the winning number or lower until the user gets it right.
-    Shows the number of attempts before correct guess.
+	"""Prompts the user to input an int guess between 1 and 10,
+	then guiding them with a feedback showing it the guess is higher than the winning number or lower until the user gets it right.
+	Shows the number of attempts before correct guess.
 	Keeps count of the best attempts score and allows multiple games to beat best score
-    """
+	"""
     # write your code inside this function.
-    welcome_message = "*****WELCOME TO NUMBER GUESSING GAME*****\n\n"
-    print(welcome_message)
+	welcome_message = "*****WELCOME TO NUMBER GUESSING GAME*****\n\n"
+	print(welcome_message)
 
 	play = 'Y'
-    best_score = 10
+	best_score = float('inf')  # Initialize best score with infinity
 
 	while play.upper() == 'Y':
 		jackpot = random.randint(1,10)
-    	guess = 0
-    	attempts = 0
-		print(f"The best score to beat is {best_score} attempts")
+		guess = 0
+		attempts = 0
+		print(f"The best score to beat is {best_score} attempts" if best_score != float('inf') else "No best score yet. You could set it!")
 		
-	    while guess != jackpot:
-	        try:
-	            guess = int(input("what's your guess (1 - 10)? "))
-	            if guess > 10 or guess < 1:
-	                raise GuessErrorCustom("the guess should be an integer between 1 and 10")
-	        except GuessErrorCustom as e:
-	            print(f'invalid input! {e}')
-	            continue
-	        except ValueError:
-	            print("Invalid input! Please enter an integer number.")
-	            continue
+		while guess != jackpot:
+			try:
+				guess = int(input("what's your guess (1 - 10)? "))
+				if guess > 10 or guess < 1:
+					raise GuessErrorCustom("the guess should be an integer between 1 and 10")
+			except GuessErrorCustom as e:
+				print(f'invalid input! {e}')
+				continue
+			except ValueError:
+				print("Invalid input! Please enter an integer number.")
+				continue
 	
-	        if guess > jackpot:
-	            print("it's lower")
-	        elif guess < jackpot:
-	            print("it's higher")
-	        attempts +=1
+			if guess > jackpot:
+				print("it's lower")
+			elif guess < jackpot:
+				print("it's higher")
+			attempts +=1
 
 		if attempts < best_score:
 			best_score = attempts
@@ -87,8 +60,8 @@ def start_game():
 
 		play = input("Would you like to play again (Y or N)? ")
 
-    exit_message = "\n\n***** GAME OVER - Goodbye! *****"
-    print(exit_message)
+	exit_message = "\n\n***** GAME OVER - Goodbye! *****"
+	print(exit_message)
 
-# Kick off the program by calling the start_game function.
-start_game()
+if __name__ == "__main__":
+	start_game()
