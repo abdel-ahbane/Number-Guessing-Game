6415612920cdeaf9b4834674fc8d5c6d4b131c39
Abdelhamid Ahbane <108112325+abdel-ahbane@users.noreply.github.com>
2025-04-25 09:53:46 +0200

    implemented Error and Exception Handling
    Basic functionality of the game with input validation

--- guessing_game.py
+++ guessing_game.py
@@ -29,6 +29,12 @@ Project 1 - The Number Guessing Game
 import random
 
 
+class GuessErrorCustom(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
 def start_game():
     """Prompts the user to input an int guess between 1 to 10,
     then guiding them with a feedback showing it the guess is higher then the winning number or lower until the user gets it right.
@@ -44,7 +50,17 @@ def start_game():
     attempts = 0
 	
     while guess != jackpot:
-        guess = int(input("what's your guess (1 - 10)? "))
+        try:
+            guess = int(input("what's your guess (1 - 10)? "))
+            if guess > 10 or guess < 1:
+                raise GuessErrorCustom("the guess should be an integer between 1 and 10")
+        except GuessErrorCustom as e:
+            print(f'invalid input! {e}')
+            continue
+        except ValueError:
+            print("Invalid input! Please enter an integer number.")
+            continue
+
         if guess > jackpot:
             print("it's lower")
         elif guess < jackpot:
